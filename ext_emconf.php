964fa29c6357f5548dc89c226be399465f4a330c
Christian Reifenscheid <christian.reifenscheid@599media.de>
2024-01-25 10:34:36 +0100

    [TASK] Configuration
    Adjusts dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'container' => '2.0.0-2.9.99'
+            'typo3' => '12.4.0-12.4.99',
+            'container' => '2.1.0-2.9.99'
         ]
     ],
     'autoload' => [
