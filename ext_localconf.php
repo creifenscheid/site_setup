fb87e838bcaabd0e673b0282f040b7355d434567
Christian Reifenscheid <dev@christian-reifenscheid.de>
2024-11-30 20:22:11 +0100

    [TASK] ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,20 +3,15 @@
 defined('TYPO3') || die();
 
 (static function ($extKey) {
+
     // Register ViewHelper
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces'][\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToLowerCamelCase($extKey)] = [
         'CReifenscheid\\' . \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey) . '\ViewHelpers',
     ];
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey),
-        'LimitedPages',
-        [
-            \CReifenscheid\SiteSetup\Controller\PagesController::class => 'list',
-        ]
-    );
 
     // RTE
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['siteSetup'] = 'EXT:site_setup/Configuration/RTE/Configuration.yaml';
     $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets'][$extKey]
         = 'EXT:' . $extKey . '/Resources/Public/Css/Abbreviation.css';
+        
 })('site_setup');
